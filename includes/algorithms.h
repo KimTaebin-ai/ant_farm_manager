301dae8520d45aa07401554d83c3442c5d4f3209
Taebin Kim <taebkim@c2r7s2.42gyeongsan.kr>
2026-05-08 11:34:29 +0900

    test

--- includes/algorithms.h
+++ includes/algorithms.h
@@ -6,7 +6,7 @@
 /*   By: taebkim <taebkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 17:37:35 by taebkim           #+#    #+#             */
-/*   Updated: 2026/05/07 21:05:55 by taebkim          ###   ########.fr       */
+/*   Updated: 2026/05/08 11:26:37 by taebkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,15 @@
 
 #include "struct.h"
 
-typedef struct s_queue {
-    t_room **data;
-    int head;
-    int tail;
-    int size;
-} t_queue;
-
-typedef struct s_path {
-    t_room **rooms;    // 경로 상의 방 배열
-    int length;        // 경로 길이 (방 개수)
-    int assigned_ants; // 이 경로에 할당된 개미 수 (나중에 쓸 것)
-} t_path;
-
-typedef struct s_ant {
-    int id;
-    t_path *path;
-    int position;
-} t_ant;
-
 t_queue *queue_create(int capacity);
 void queue_destroy(t_queue *q);
 void enqueue(t_queue *q, t_room *room);
 t_room *dequeue(t_queue *q);
 int queue_is_empty(t_queue *q);
 
+// utils
 void reset_visited(t_farm *farm);
+int residual_capacity(t_link *link);
 
 // for ant
 t_ant *create_ants(t_farm *farm, t_path *path);
--- src/algorithms/utils.c
+++ src/algorithms/utils.c
@@ -6,13 +6,23 @@
 /*   By: taebkim <taebkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 17:39:57 by taebkim           #+#    #+#             */
-/*   Updated: 2026/05/07 20:56:14 by taebkim          ###   ########.fr       */
+/*   Updated: 2026/05/08 11:22:37 by taebkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "algorithms.h"
 #include "ft_printf.h"
+
+int can_use_room(t_room *room) {
+    if (room->is_start || room->is_end)
+        return 1;
+    return room->in_flow == 0;
+}
+
+int residual_capacity(t_link *link) {
+    return (link->capacity - link->flow);
+}
 
 void reset_visited(t_farm *farm) {
     t_room *current = farm->rooms;
